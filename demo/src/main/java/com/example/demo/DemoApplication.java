ff8c3399a16c6d4871e4815a5d21a2080f9bf776
jte91 <j3sh0n3dward5@gmail.com>
2022-11-02 19:16:46 -0500

    student api layer

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,18 @@
 package com.example.demo;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+// import java.time.LocalDate;
+// import java.time.Month;
+// import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.Student.Student;
+// import com.example.demo.Student.Student;
 
 @SpringBootApplication
-@RestController
+// @RestController
 public class DemoApplication {
 
 	public static void main(String[] args)
@@ -20,15 +20,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-    @GetMapping
-    public List<Student> hello()
-    {
-        return List.of(new Student(
-            1L,
-            "Mariam",
-            "mariam.jamal@gmail.com",
-            LocalDate.of(2000,Month.JANUARY,5),
-            21
-        ) ) ;
-    }
+    // @GetMapping
+    // public List<Student> hello()
+    // {
+    //     return List.of(new Student(
+    //         1L,
+    //         "Mariam",
+    //         "mariam.jamal@gmail.com",
+    //         LocalDate.of(2000,Month.JANUARY,5),
+    //         21
+    //     ) ) ;
+    // }
 }
