3cb59fd6c5572874fb4dc19d955ce2d8f6f95d38
jte91 <j3sh0n3dward5@gmail.com>
2022-11-02 20:03:18 -0500

    JPA and @Entry

--- demo/src/main/java/com/example/demo/Student/Student.java
+++ demo/src/main/java/com/example/demo/Student/Student.java
@@ -2,18 +2,37 @@ package com.example.demo.Student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Entity
+@Table
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Student {
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
