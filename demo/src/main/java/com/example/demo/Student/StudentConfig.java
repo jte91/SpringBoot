41a53103cfd7e34100b7450711ae2850f94165a9
jte91 <j3sh0n3dward5@gmail.com>
2022-11-02 20:25:36 -0500

    saving students

---
+++ demo/src/main/java/com/example/demo/Student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+    
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+                null, "Mariam",
+                "mariam.jamal@gmail.com", 
+                LocalDate.of(2000,Month.JANUARY,5), null
+            );
+
+            Student alex = new Student(
+                null, "Alex",
+                "alex@gmail.com", 
+                LocalDate.of(2004,Month.JANUARY,5), null
+            );
+
+            repository.saveAll(
+                List.of(mariam, alex)
+                );
+        };
+    }
+}
